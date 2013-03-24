a94495539ffdad944571ecc817469cd5533956d2
Chris Thomas <christhomas42@wpi.edu>
2013-03-24 11:06:58 -0400

    modfied triangulation.py
    no longer asks for desired resolution. Always assumes best resolution is
    desired.

--- triangulation.py
+++ triangulation.py
@@ -7,31 +7,21 @@ def triangulation():
     ratio = input('Enter the projector throw ratio(%): ')
     rp = input('Enter the x-resolution of the projector(pixels): ')
     throw = input('Enter the minimum throw distance of projector(m) ')
-    x = input('Enter the desired spatial resolution(m) ')
     tri = input('Enter the triangulation angle(degrees) ')
     #conveting from percent to decimal
     ratio = ratio/100.0
     #converting to radians
+    maxres = throw/(ratio*rp)
     thetao = (tri*numpy.pi)/180
-    zc = fc*(x/uc)
-    zp = x*ratio*rp
-    #Checking if the distance from object to projector is less than the minimum
-    #throw distance
-    if zp < throw:
-        #Output min spatial resolution
-        print
-        print('Desired spatial resolution is not atainable with selected projector')
-        maxres = throw/(ratio*rp)
-        print('The smallest attainable spatial resolution is ' + str(maxres) + ' meters/pixel')
-
-    else:
-        #Calculating the distance between camera and projector
-        D = (zc**2 + zp**2 - zc*zc*numpy.cos(thetao))**0.5
-        #Output distances
-        print
-        print('Distance from projector to target: ' +str(zp) + ' meters')
-        print('Distance from camera to target: ' +str(zc) + ' meters')
-        print('Distance between camera and projector: ' +str(D) + ' meters')
+    zc = fc*(maxres/uc)
+    zp = maxres*ratio*rp  
+    #Calculating the distance between camera and projector
+    D = (zc**2 + zp**2 - zc*zc*numpy.cos(thetao))**0.5
+    #Output distances
+    print
+    print('Distance from projector to target: ' +str(zp) + ' meters')
+    print('Distance from camera to target: ' +str(zc) + ' meters')
+    print('Distance between camera and projector: ' +str(D) + ' meters')
         
         
         
